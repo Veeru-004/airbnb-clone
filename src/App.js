bc1ad32c49c4af190c901020a633b4cb2ab1cb9f
Machani Veeresh Babu <81911109+Veeru-004@users.noreply.github.com>
2022-06-25 13:14:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import Card from "../Components/Card";
+import data from "./data";
+export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
+}
